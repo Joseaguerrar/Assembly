e2d5973f98a623eae139eafaa6b62185cffd38c1
joseaguerrar <joseaguerrar2017@gmail.com>
2024-09-15 23:21:48 -0600

    Implementación del ciclo completo

--- tarea_corta3/ejercicio_2/ejercicio_2.asm
+++ tarea_corta3/ejercicio_2/ejercicio_2.asm
@@ -25,3 +25,56 @@ section .text
 _start:
     ; Llamar al método para ejecutar la criba de Eratóstenes
     call criba_eratostenes
+
+    ; Finalizar el programa y devolver control al SO
+    mov eax, 60 ; sys_exit
+    xor edi, edi
+    syscall
+
+criba_eratostenes:
+    ;Inicializar variables 
+    xor ecx, ecx    ;índice de la matriz
+    xor ebx, ebx    ;índice del resultado de primos
+    xor esi, esi    ;Número actual en la criba
+
+    ; Empezar la criba
+criba_loop:
+    ; Cargar el número en la posición actual de la matriz
+    mov eax, [matriz + ecx*4]
+
+    ; Si el número es 0, ha sido marcado como no primo, continuar con el siguiente
+    cmp eax, 0
+    je siguiente_numero
+
+    ; Si el número es mayor que la raíz de 101, paramos
+
+    cmp eax, 11 ;la raíz de 101 es aprox 10
+    jg almacenar_primo
+
+
+    ;Marcar los múltiplos del número actual como no primos mov esi, ecx
+marcar_multiplos:
+    ;Incrementar el índice para obtener el múltiplo
+    add esi, eax
+    cmp esi, 99 ;No sobrepasar el tamaño de la matriz
+    jg fin_marcar
+
+    ;Marcar el múltiplo como no primo(poner a 0)
+    mov dword[matriz + esi*4], 0
+    jmp marcar_multiplos
+
+fin_marcar:
+    ;Almacenar el número primo encontrado
+almacenar_primo:
+    mov eax, [matriz + ecx*4]
+    mov [resultado + ebx*4], eax
+    inc ebx     ;Incrementar índice del resultado
+    mov [num_primos], ebx   ;Actualizar primos encontrados
+
+    ;siguiente número en la matriz
+siguiente_numero:
+    inc ecx
+    cmp ecx, 100
+    jl cripa_loop
+
+    ret
