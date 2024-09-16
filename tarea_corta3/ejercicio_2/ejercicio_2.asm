12a17bf48eccc7c0c5066b986dcca791dd54dfad
joseaguerrar <joseaguerrar2017@gmail.com>
2024-09-15 22:49:35 -0600

    section bss y data definidas, primera llamada en el ciclo start, falta implementar

--- tarea_corta3/ejercicio_2/ejercicio_2.asm
+++ tarea_corta3/ejercicio_2/ejercicio_2.asm
@@ -10,3 +10,18 @@ matriz     dd  2, 3, 4, 5, 6, 7, 8, 9, 10, 11
            dd  72, 73, 74, 75, 76, 77, 78, 79, 80, 81
            dd  82, 83, 84, 85, 86, 87, 88, 89, 90, 91
            dd  92, 93, 94, 95, 96, 97, 98, 99, 100, 101
+; Vector para almacenar los números primos encontrados
+resultado resd 25 ;que es suficiente para los primos menores o iguales a 101
+
+;variable para contar la cantidad de primos encontrados
+num_primos dd 0
+
+section .bss
+; Variable para el índice
+idx resd 1
+
+section .text
+    global _start
+_start:
+    ; Llamar al método para ejecutar la criba de Eratóstenes
+    call criba_eratostenes
