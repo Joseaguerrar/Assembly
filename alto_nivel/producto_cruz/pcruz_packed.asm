d739ac98fefe8418b33cdc38529f4d4f255c24fa
joseaguerrar <joseaguerrar2017@gmail.com>
2024-10-23 23:54:51 -0600

    Código completo y funcional de empaquetado de vectores para el producto cruz

--- alto_nivel/producto_cruz/pcruz_packed.asm
+++ alto_nivel/producto_cruz/pcruz_packed.asm
@@ -1,36 +1,66 @@
-section .data
-    resultado: times 24 dd 0.0  ; Espacio para el resultado de 8 productos cruz (3 componentes por vector)
-
 section .text
 global producto_cruz_packed
 producto_cruz_packed:
-    ; Primer conjunto de 8 vectores
-    vmovaps ymm0, [rdi]        ; a1 (8 valores empaquetados)
-    vmovaps ymm1, [rdi+32]     ; a2 (8 valores empaquetados)
-    vmovaps ymm2, [rdi+64]     ; a3 (8 valores empaquetados)
+    ; Inicializar contador en 8 para procesar 8 pares de vectores
+    mov rcx, 8                  ; Contador para 8 iteraciones
 
-    ; Segundo conjunto de 8 vectores
-    vmovaps ymm3, [rsi]        ; b1 (8 valores empaquetados)
-    vmovaps ymm4, [rsi+32]     ; b2 (8 valores empaquetados)
-    vmovaps ymm5, [rsi+64]     ; b3 (8 valores empaquetados)
+procesar_vectores:
+    ; Calcular c1 = a2*b3 - a3*b2
+    vmovss xmm0, [rdi+4]        ; a2
+    vmovss xmm1, [rsi+8]        ; b3
+    vmulss xmm2, xmm0, xmm1     ; xmm2 = a2 * b3
+    vmovss [rdx], xmm2          ; Guardar a2 * b3 en resultado
 
-    ; Calcular c1 para los 8 pares de vectores
-    vmulps ymm6, ymm1, ymm5    ; a2 * b3
-    vmulps ymm7, ymm2, ymm4    ; a3 * b2
-    vsubps ymm8, ymm6, ymm7    ; a2*b3 - a3*b2
-    vmovaps [resultado], ymm8  ; Guardar c1
+    vmovss xmm0, [rdi+8]        ; a3
+    vmovss xmm1, [rsi+4]        ; b2
+    vmulss xmm2, xmm0, xmm1     ; xmm2 = a3 * b2
+    vmovss [rdx+4], xmm2        ; Guardar a3 * b2 en resultado
 
-    ; Calcular c2 para los 8 pares de vectores
-    vmulps ymm6, ymm2, ymm3    ; a3 * b1
-    vmulps ymm7, ymm0, ymm5    ; a1 * b3
-    vsubps ymm8, ymm6, ymm7    ; a3*b1 - a1*b3
-    vmovaps [resultado+32], ymm8  ; Guardar c2
+    vmovss xmm0, [rdx]          ; Cargar a2 * b3
+    vmovss xmm1, [rdx+4]        ; Cargar a3 * b2
+    vsubss xmm2, xmm0, xmm1     ; xmm2 = a2*b3 - a3*b2
+    vmovss [rdx], xmm2          ; Guardar c1 en resultado
 
-    ; Calcular c3 para los 8 pares de vectores
-    vmulps ymm6, ymm0, ymm4    ; a1 * b2
-    vmulps ymm7, ymm1, ymm3    ; a2 * b1
-    vsubps ymm8, ymm6, ymm7    ; a1*b2 - a2*b1
-    vmovaps [resultado+64], ymm8  ; Guardar c3
+    ; Calcular c2 = a3*b1 - a1*b3
+    vmovss xmm0, [rdi+8]        ; a3
+    vmovss xmm1, [rsi]          ; b1
+    vmulss xmm2, xmm0, xmm1     ; xmm2 = a3 * b1
+    vmovss [rdx+4], xmm2        ; Guardar a3 * b1 en resultado
 
-    mov rax, resultado
+    vmovss xmm0, [rdi]          ; a1
+    vmovss xmm1, [rsi+8]        ; b3
+    vmulss xmm2, xmm0, xmm1     ; xmm2 = a1 * b3
+    vmovss [rdx+8], xmm2        ; Guardar a1 * b3 en resultado
+
+    vmovss xmm0, [rdx+4]        ; Cargar a3 * b1
+    vmovss xmm1, [rdx+8]        ; Cargar a1 * b3
+    vsubss xmm2, xmm0, xmm1     ; xmm2 = a3*b1 - a1*b3
+    vmovss [rdx+4], xmm2        ; Guardar c2 en resultado
+
+    ; Calcular c3 = a1*b2 - a2*b1
+    vmovss xmm0, [rdi]          ; a1
+    vmovss xmm1, [rsi+4]        ; b2
+    vmulss xmm2, xmm0, xmm1     ; xmm2 = a1 * b2
+    vmovss [rdx+8], xmm2        ; Guardar a1 * b2 en resultado
+
+    vmovss xmm0, [rdi+4]        ; a2
+    vmovss xmm1, [rsi]          ; b1
+    vmulss xmm2, xmm0, xmm1     ; xmm2 = a2 * b1
+    vmovss [rdx+12], xmm2       ; Guardar a2 * b1 en resultado
+
+    vmovss xmm0, [rdx+8]        ; Cargar a1 * b2
+    vmovss xmm1, [rdx+12]       ; Cargar a2 * b1
+    vsubss xmm2, xmm0, xmm1     ; xmm2 = a1*b2 - a2*b1
+    vmovss [rdx+8], xmm2        ; Guardar c3 en resultado
+
+    ; Avanzar punteros rdi, rsi y rdx para la siguiente iteración
+    add rdx, 12                 ; Avanzar 12 bytes para almacenar el siguiente vector
+    add rdi, 12                 ; Avanzar 12 bytes para los siguientes valores de vector1
+    add rsi, 12                 ; Avanzar 12 bytes para los siguientes valores de vector2
+
+    ; Decrementar el contador y verificar si seguimos iterando
+    dec rcx                     ; Decrementar el contador
+    jnz procesar_vectores        ; Si rcx no es cero, repetir el bucle
+
+    mov rax, rdx                ; Retornar la dirección del resultado
     ret
--- alto_nivel/producto_cruz/pcruz_packed.cpp
+++ alto_nivel/producto_cruz/pcruz_packed.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <fstream>
+#include <cstdlib>  // Para aligned_alloc y free
 
 using namespace std;
 
-extern "C" float* producto_cruz_packed(float[], float[]);
+extern "C" void producto_cruz_packed(float[], float[], float[]);
 
 int main () {
     ifstream archivo_entrada("vectors.txt");
@@ -16,19 +17,49 @@ int main () {
 
     // Asumiendo que vamos a procesar bloques de 8 vectores
     const int VECTOR_SIZE = 3 * 8;
-    float vector1[VECTOR_SIZE];
-    float vector2[VECTOR_SIZE];
-    float* resultado = new float[VECTOR_SIZE];
+    float* vector1 = (float*)aligned_alloc(32, VECTOR_SIZE * sizeof(float));
+    float* vector2 = (float*)aligned_alloc(32, VECTOR_SIZE * sizeof(float));
+    float* resultado = (float*)aligned_alloc(32, VECTOR_SIZE * sizeof(float));
+
+    if (vector1 == nullptr || vector2 == nullptr || resultado == nullptr) {
+        cout << "Error al asignar memoria" << endl;
+        return 1;
+    }
 
     while (archivo_entrada >> vector1[0] >> vector1[1] >> vector1[2] >> vector2[0] >> vector2[1] >> vector2[2]) {
-        // Leer los 8 vectores en cada ciclo
-        for (int i = 0; i < VECTOR_SIZE; i += 3) {
-            archivo_entrada >> vector1[i] >> vector1[i+1] >> vector1[i+2];
-            archivo_entrada >> vector2[i] >> vector2[i+1] >> vector2[i+2];
-        }
-        
-        resultado = producto_cruz_packed(vector1, vector2);
+        bool datos_completos = true;
 
+        for (int i = 3; i < VECTOR_SIZE; i += 3) {
+            if (!(archivo_entrada >> vector1[i] >> vector1[i+1] >> vector1[i+2])) {
+                datos_completos = false;
+                break;
+            }
+            if (!(archivo_entrada >> vector2[i] >> vector2[i+1] >> vector2[i+2])) {
+                datos_completos = false;
+                break;
+            }
+        }
+
+        if (!datos_completos) {
+            cout << "Datos insuficientes en el archivo para llenar los 8 vectores." << endl;
+            break;
+        }
+
+        // Depuración: Imprimir los valores de los vectores leídos
+        cout << "Valores de vector1:" << endl;
+        for (int i = 0; i < VECTOR_SIZE; i += 3) {
+            cout << vector1[i] << ", " << vector1[i+1] << ", " << vector1[i+2] << endl;
+        }
+
+        cout << "Valores de vector2:" << endl;
+        for (int i = 0; i < VECTOR_SIZE; i += 3) {
+            cout << vector2[i] << ", " << vector2[i+1] << ", " << vector2[i+2] << endl;
+        }
+
+        // Llamar a la función ensamblador
+        producto_cruz_packed(vector1, vector2, resultado);
+
+        // Guardar el resultado en el archivo
         for (int i = 0; i < VECTOR_SIZE; i += 3) {
             archivo_salida << resultado[i] << ", " << resultado[i+1] << ", " << resultado[i+2] << endl; 
         }
@@ -37,5 +68,11 @@ int main () {
     archivo_entrada.close();
     archivo_salida.close();
     cout << "Listo" << endl;
+
+    // Usar free() para liberar la memoria alineada
+    free(vector1);
+    free(vector2);
+    free(resultado);
+
     return 0;
 }
