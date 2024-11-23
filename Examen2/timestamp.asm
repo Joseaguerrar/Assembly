8cc31a04f3070f364e339757755c78db70646733
joseaguerrar <joseaguerrar2017@gmail.com>
2024-11-23 11:40:25 -0600

    codificación incluyendo el mes

--- Examen2/timestamp.asm
+++ Examen2/timestamp.asm
@@ -1,9 +1,15 @@
 section .text
-    global generateDayTimestamp
+    global generateTimestamp
 
-generateDayTimestamp:
-    ; timeOfDay está en RDI y dayOfWeek en RSI
-    shl rdi, 3         ; Mueve timeOfDay 3 bits a la izquierda (para dejar espacio para el día)
-    or rdi, rsi        ; Combina ambos valores (timeOfDay en el bit 3, dayOfWeek en los bits 0-2)
+generateTimestamp:
+    ; timeOfDay está en RDI
+    ; dayOfWeek está en RSI
+    ; month está en RDX
+
+    shl rdi, 7         ; Mueve el momento del día al bit 7
+    shl rdx, 4         ; Mueve el mes a los bits 6-4
+    or rdi, rdx        ; Combina el momento del día y el mes
+    or rdi, rsi        ; Combina el día de la semana
+
     mov eax, edi       ; Copia el resultado combinado a EAX
     ret                ; Devuelve el resultado
