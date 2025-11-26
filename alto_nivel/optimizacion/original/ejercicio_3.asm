ea9668f3c9e648e00a71dab6eebdff028912338e
Jose Guerra Rodríguez <163485375+Joseaguerrar@users.noreply.github.com>
2025-11-26 13:40:13 -0600

    Remove leading empty line in ejercicio_3.asm
    Removed an empty line at the beginning of the file.

--- alto_nivel/optimizacion/original/ejercicio_3.asm
+++ alto_nivel/optimizacion/original/ejercicio_3.asm
@@ -1,7 +1,6 @@
-; Ejercicio 3: Jerson Bonilla Fernández (C31225)
 section .data
-    ; Definimos la matriz original en la sección de datos (4x4)
-    matriz:     dw 1, 2, 3, 4
+    ; Define the original matrix in the data section (4x4)
+    matrix:     dw 1, 2, 3, 4
                 dw 5, 6, 7, 8
                 dw 9, 10, 11, 12
                 dw 13, 14, 15, 16
@@ -10,55 +9,55 @@ section .text
     global _start
 
 _start:
-    ; Llamar al método para intercambiar los cuadrantes
-    call intercambiar_cuadrantes
+    ; Call the method to swap the quadrants
+    call swap_quadrants
 
-    ; Salir del programa
+    ; Exit the program
     mov rax, 60           ; syscall: exit
-    xor rdi, rdi          ; código de salida 0
+    xor rdi, rdi          ; exit code 0
     syscall
 
-intercambiar_cuadrantes:
-    ; Intercambiar el cuadrante superior izquierdo con el inferior derecho
-    mov ax, [matriz + 0*2]  ; ax = matriz[0]
-    mov bx, [matriz + 10*2] ; bx = matriz[10]
-    mov [matriz + 0*2], bx  ; matriz[0] = matriz[10]
-    mov [matriz + 10*2], ax ; matriz[10] = ax
+swap_quadrants:
+    ; Swap upper-left quadrant with lower-right quadrant
+    mov ax, [matrix + 0*2]  ; ax = matrix[0]
+    mov bx, [matrix + 10*2] ; bx = matrix[10]
+    mov [matrix + 0*2], bx  ; matrix[0] = matrix[10]
+    mov [matrix + 10*2], ax ; matrix[10] = ax
 
-    mov ax, [matriz + 1*2]  ; ax = matriz[1]
-    mov bx, [matriz + 11*2] ; bx = matriz[11]
-    mov [matriz + 1*2], bx  ; matriz[1] = matriz[11]
-    mov [matriz + 11*2], ax ; matriz[11] = ax
+    mov ax, [matrix + 1*2]  ; ax = matrix[1]
+    mov bx, [matrix + 11*2] ; bx = matrix[11]
+    mov [matrix + 1*2], bx  ; matrix[1] = matrix[11]
+    mov [matrix + 11*2], ax ; matrix[11] = ax
 
-    mov ax, [matriz + 2*2]  ; ax = matriz[2]
-    mov bx, [matriz + 8*2]  ; bx = matriz[8]
-    mov [matriz + 2*2], bx  ; matriz[2] = matriz[8]
-    mov [matriz + 8*2], ax  ; matriz[8] = ax
+    mov ax, [matrix + 2*2]  ; ax = matrix[2]
+    mov bx, [matrix + 8*2]  ; bx = matrix[8]
+    mov [matrix + 2*2], bx  ; matrix[2] = matrix[8]
+    mov [matrix + 8*2], ax  ; matrix[8] = ax
 
-    mov ax, [matriz + 3*2]  ; ax = matriz[3]
-    mov bx, [matriz + 9*2]  ; bx = matriz[9]
-    mov [matriz + 3*2], bx  ; matriz[3] = matriz[9]
-    mov [matriz + 9*2], ax  ; matriz[9] = ax
+    mov ax, [matrix + 3*2]  ; ax = matrix[3]
+    mov bx, [matrix + 9*2]  ; bx = matrix[9]
+    mov [matrix + 3*2], bx  ; matrix[3] = matrix[9]
+    mov [matrix + 9*2], ax  ; matrix[9] = ax
 
-    ; Intercambiar el cuadrante superior derecho con el inferior izquierdo
-    mov ax, [matriz + 4*2]  ; ax = matriz[4]
-    mov bx, [matriz + 12*2] ; bx = matriz[12]
-    mov [matriz + 4*2], bx  ; matriz[4] = matriz[12]
-    mov [matriz + 12*2], ax ; matriz[12] = ax
+    ; Swap upper-right quadrant with lower-left quadrant
+    mov ax, [matrix + 4*2]  ; ax = matrix[4]
+    mov bx, [matrix + 12*2] ; bx = matrix[12]
+    mov [matrix + 4*2], bx  ; matrix[4] = matrix[12]
+    mov [matrix + 12*2], ax ; matrix[12] = ax
 
-    mov ax, [matriz + 5*2]  ; ax = matriz[5]
-    mov bx, [matriz + 13*2] ; bx = matriz[13]
-    mov [matriz + 5*2], bx  ; matriz[5] = matriz[13]
-    mov [matriz + 13*2], ax ; matriz[13] = ax
+    mov ax, [matrix + 5*2]  ; ax = matrix[5]
+    mov bx, [matrix + 13*2] ; bx = matrix[13]
+    mov [matrix + 5*2], bx  ; matrix[5] = matrix[13]
+    mov [matrix + 13*2], ax ; matrix[13] = ax
 
-    mov ax, [matriz + 6*2]  ; ax = matriz[6]
-    mov bx, [matriz + 14*2] ; bx = matriz[14]
-    mov [matriz + 6*2], bx  ; matriz[6] = matriz[14]
-    mov [matriz + 14*2], ax ; matriz[14] = ax
+    mov ax, [matrix + 6*2]  ; ax = matrix[6]
+    mov bx, [matrix + 14*2] ; bx = matrix[14]
+    mov [matrix + 6*2], bx  ; matrix[6] = matrix[14]
+    mov [matrix + 14*2], ax ; matrix[14] = ax
 
-    mov ax, [matriz + 7*2]  ; ax = matriz[7]
-    mov bx, [matriz + 15*2] ; bx = matriz[15]
-    mov [matriz + 7*2], bx  ; matriz[7] = matriz[15]
-    mov [matriz + 15*2], ax ; matriz[15] = ax
+    mov ax, [matrix + 7*2]  ; ax = matrix[7]
+    mov bx, [matrix + 15*2] ; bx = matrix[15]
+    mov [matrix + 7*2], bx  ; matrix[7] = matrix[15]
+    mov [matrix + 15*2], ax ; matrix[15] = ax
 
     ret
