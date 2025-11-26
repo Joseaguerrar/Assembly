1d6cfa4edb163bd2de15c2d9d4805c4cf5f1eab9
Jose Guerra Rodríguez <163485375+Joseaguerrar@users.noreply.github.com>
2025-11-26 13:57:44 -0600

    Refactor comments in fibonacci.asm for clarity

--- alto_nivel/tarea_corta4/fibo.asm
+++ alto_nivel/tarea_corta4/fibo.asm
@@ -1,42 +1,42 @@
-; Integrantes: Jose Guerra (50%) y Jerson Bonilla (50%)
+; Members: Jose Guerra (50%) and Jerson Bonilla (50%)
 section .note.GNU-stack noalloc noexec nowrite progbits
 
 section .text
     global _fibonacci
 
 _fibonacci:
-    push rbp                 ; Guardar el frame pointer anterior
-    mov rbp, rsp             ; Establecer el nuevo frame pointer
-    push rbx                 ; Guardar el valor de rbx
-    push rsi                 ; Guardar rsi
+    push rbp                 ; Save previous frame pointer
+    mov rbp, rsp             ; Set new frame pointer
+    push rbx                 ; Save rbx
+    push rsi                 ; Save rsi
 
-    ; El valor de n está en rdi
-    cmp rdi, 1               ; Comparar n con 1
-    jbe fib_base_case        ; Si n <= 1, ir al caso base
+    ; The value of n is in rdi
+    cmp rdi, 1               ; Compare n with 1
+    jbe fib_base_case        ; If n <= 1, go to base case
 
-    ; Guardar el valor de n en rbx (temporal)
-    mov rbx, rdi             ; Guardar el valor original de n en rbx
+    ; Save n in rbx (temporary)
+    mov rbx, rdi             ; Save original value of n in rbx
 
-    ; Llamada recursiva fib(n-1)
-    sub rdi, 1               ; n = n-1
-    call _fibonacci          ; Llamada recursiva para fib(n-1)
-    mov rsi, rax             ; Guardar resultado de fib(n-1) en rsi
+    ; Recursive call fib(n-1)
+    sub rdi, 1               ; n = n - 1
+    call _fibonacci          ; Recursive call for fib(n-1)
+    mov rsi, rax             ; Store result of fib(n-1) in rsi
 
-    ; Restaurar el valor original de n desde rbx
+    ; Restore original n from rbx
     mov rdi, rbx
-    sub rdi, 2               ; n = n-2
-    call _fibonacci          ; Llamada recursiva para fib(n-2)
+    sub rdi, 2               ; n = n - 2
+    call _fibonacci          ; Recursive call for fib(n-2)
 
-    ; Sumar fib(n-1) y fib(n-2)
-    add rax, rsi             ; Sumar fib(n-1) (guardado en rsi) con fib(n-2)
+    ; Add fib(n-1) and fib(n-2)
+    add rax, rsi             ; Add fib(n-1) (stored in rsi) with fib(n-2)
 
-    jmp fib_done             ; Saltar a la salida
+    jmp fib_done             ; Jump to exit
 
 fib_base_case:
-    mov rax, rdi             ; Caso base: fib(0) = 0 o fib(1) = 1
+    mov rax, rdi             ; Base case: fib(0) = 0 or fib(1) = 1
 
 fib_done:
-    pop rsi                  ; Restaurar rsi
-    pop rbx                  ; Restaurar rbx
-    pop rbp                  ; Restaurar el frame pointer
-    ret                      ; Retornar
+    pop rsi                  ; Restore rsi
+    pop rbx                  ; Restore rbx
+    pop rbp                  ; Restore frame pointer
+    ret                      ; Return
