653714f90e7d6497f1467212560c3b46bf7fccfd
Jose Guerra Rodríguez <163485375+Joseaguerrar@users.noreply.github.com>
2025-11-26 13:41:59 -0600

    Refactor variable names and comments in assembly

--- alto_nivel/optimizacion/optimized/ejercicio_3_optimized.asm
+++ alto_nivel/optimizacion/optimized/ejercicio_3_optimized.asm
@@ -1,7 +1,6 @@
-; Ejercicio 3: Jerson Bonilla Fernández (C31225)
 section .data
-    ; Definimos la matriz original en la sección de datos (4x4)
-    matriz:     dw 1, 2, 3, 4
+    ; Define the original matrix in the data section (4x4)
+    matrix:     dw 1, 2, 3, 4
                 dw 5, 6, 7, 8
                 dw 9, 10, 11, 12
                 dw 13, 14, 15, 16
@@ -10,61 +9,61 @@ section .text
     global _start
 
 _start:
-    ; Llamar al método para intercambiar los cuadrantes
-    call intercambiar_cuadrantes
+    ; Call the method to swap the quadrants
+    call swap_quadrants
 
-    ; Salir del programa
+    ; Exit the program
     mov rax, 60           ; syscall: exit
-    xor rdi, rdi          ; código de salida 0
+    xor rdi, rdi          ; exit code 0
     syscall
 
-intercambiar_cuadrantes:
-    ; Cargamos la dirección base de la matriz en rdx para minimizar accesos a memoria
-    lea rdx, [matriz] 
+swap_quadrants:
+    ; Load the base address of the matrix into rdx to minimize memory accesses
+    lea rdx, [matrix] 
 
-    ; Intercambio del cuadrante superior izquierdo con el inferior derecho, usando acumuladores adicionales
-    mov rax, [rdx + 0*2]     ; Cargamos matriz[0] en rax
-    mov rbx, [rdx + 10*2]    ; Cargamos matriz[10] en rbx
-    mov rcx, [rdx + 1*2]     ; Cargamos matriz[1] en rcx
-    mov rdi, [rdx + 11*2]    ; Cargamos matriz[11] en rdi
+    ; Swap the upper-left quadrant with the lower-right quadrant, using additional accumulators
+    mov rax, [rdx + 0*2]     ; Load matrix[0] into rax
+    mov rbx, [rdx + 10*2]    ; Load matrix[10] into rbx
+    mov rcx, [rdx + 1*2]     ; Load matrix[1] into rcx
+    mov rdi, [rdx + 11*2]    ; Load matrix[11] into rdi
 
-    ; Realizamos el intercambio sin cadenas largas de dependencia
-    mov [rdx + 0*2], rbx     ; matriz[0] = matriz[10]
-    mov [rdx + 10*2], rax    ; matriz[10] = matriz[0]
-    mov [rdx + 1*2], rdi     ; matriz[1] = matriz[11]
-    mov [rdx + 11*2], rcx    ; matriz[11] = matriz[1]
+    ; Perform the swap without long dependency chains
+    mov [rdx + 0*2], rbx     ; matrix[0] = matrix[10]
+    mov [rdx + 10*2], rax    ; matrix[10] = matrix[0]
+    mov [rdx + 1*2], rdi     ; matrix[1] = matrix[11]
+    mov [rdx + 11*2], rcx    ; matrix[11] = matrix[1]
 
-    ; Continuamos con el mismo patrón para los siguientes elementos
+    ; Continue with the same pattern for the next elements
     mov rax, [rdx + 2*2]
     mov rbx, [rdx + 8*2]
     mov rcx, [rdx + 3*2]
     mov rdi, [rdx + 9*2]
 
-    mov [rdx + 2*2], rbx     ; matriz[2] = matriz[8]
-    mov [rdx + 8*2], rax     ; matriz[8] = matriz[2]
-    mov [rdx + 3*2], rdi     ; matriz[3] = matriz[9]
-    mov [rdx + 9*2], rcx     ; matriz[9] = matriz[3]
+    mov [rdx + 2*2], rbx     ; matrix[2] = matrix[8]
+    mov [rdx + 8*2], rax     ; matrix[8] = matrix[2]
+    mov [rdx + 3*2], rdi     ; matrix[3] = matrix[9]
+    mov [rdx + 9*2], rcx     ; matrix[9] = matrix[3]
 
-    ; Intercambio del cuadrante superior derecho con el inferior izquierdo
+    ; Swap the upper-right quadrant with the lower-left quadrant
     mov rax, [rdx + 4*2]
     mov rbx, [rdx + 12*2]
     mov rcx, [rdx + 5*2]
     mov rdi, [rdx + 13*2]
 
-    mov [rdx + 4*2], rbx     ; matriz[4] = matriz[12]
-    mov [rdx + 12*2], rax    ; matriz[12] = matriz[4]
-    mov [rdx + 5*2], rdi     ; matriz[5] = matriz[13]
-    mov [rdx + 13*2], rcx    ; matriz[13] = matriz[5]
+    mov [rdx + 4*2], rbx     ; matrix[4] = matrix[12]
+    mov [rdx + 12*2], rax    ; matrix[12] = matrix[4]
+    mov [rdx + 5*2], rdi     ; matrix[5] = matrix[13]
+    mov [rdx + 13*2], rcx    ; matrix[13] = matrix[5]
 
-    ; Continuamos con el mismo patrón para los últimos elementos
+    ; Continue with the same pattern for the last elements
     mov rax, [rdx + 6*2]
     mov rbx, [rdx + 14*2]
     mov rcx, [rdx + 7*2]
     mov rdi, [rdx + 15*2]
 
-    mov [rdx + 6*2], rbx     ; matriz[6] = matriz[14]
-    mov [rdx + 14*2], rax    ; matriz[14] = matriz[6]
-    mov [rdx + 7*2], rdi     ; matriz[7] = matriz[15]
-    mov [rdx + 15*2], rcx    ; matriz[15] = matriz[7]
+    mov [rdx + 6*2], rbx     ; matrix[6] = matrix[14]
+    mov [rdx + 14*2], rax    ; matrix[14] = matrix[6]
+    mov [rdx + 7*2], rdi     ; matrix[7] = matrix[15]
+    mov [rdx + 15*2], rcx    ; matrix[15] = matrix[7]
 
     ret
